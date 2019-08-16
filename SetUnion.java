108212a36cbc6e069660ae07a84f3b2c5b0eb264
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-15 21:49:49 -0400

    Add files via upload

--- SetUnion.java
+++ SetUnion.java
@@ -6,17 +6,28 @@ import java.util.Set;
 public class SetUnion {
 
 	public static void main(String[] args) {
+		
+		
 		Set<Integer> s1=new HashSet<Integer>();
 		for(int i=0;i<10;i++) {
 			s1.add(i);
+		}
+		System.out.println("The first set:");
+		for(Integer i:s1) {
+			System.out.print(i+",");
 		}
 		Set<Integer> s2=new HashSet<Integer>();
 		for(int i=5;i<15;i++) {
 			s2.add(i);
 		}
+		System.out.println("\nThe second set:");
+		for(Integer i:s2) {
+			System.out.print(i+",");
+		}
 		s1.addAll(s2);
+		System.out.println("\nUnion of these sets is:");
 		for(Integer i:s1) {
-			System.out.println(i);
+			System.out.print(i+",");
 		}
 	}
 
